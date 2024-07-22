4c7c60e0e99e05cb24fe6bf86fe8d952ec772910
Felipo <gabriel.felipo2010@gmail.com>
2024-07-22 12:24:34 -0300

    feat(authentication): remove dist

--- backend/dist/src/common/errors/unauthoraized-error.d.ts
+++
@@ -1,7 +0,0 @@
-import { DomainError, ErrorCodes } from '.';
-export declare class UnauthorizedError implements DomainError {
-    error: string;
-    code: ErrorCodes;
-    response: unknown;
-    constructor(message?: string);
-}
