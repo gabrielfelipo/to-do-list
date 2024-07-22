e7c02aaf58cdcae6afe4fd04dfe8e533173d4556
Felipo <gabriel.felipo2010@gmail.com>
2024-07-22 09:25:38 -0300

    feat(authentication): add prisma member mapper and repository

---
+++ backend/src/common/prisma/mappers/member.mapper.ts
@@ -0,0 +1,25 @@
+import { Prisma, Member as MemberRaw } from '@prisma/client'
+import { Member } from 'src/modules/member/entities/Member'
+import { nullsToUndefined } from 'src/utils/nulls-to-undefined'
+import { SetOptional } from 'type-fest'
+
+export class MemberMapper {
+  static toDomain(
+    raw_: SetOptional<
+      Prisma.MemberGetPayload<{ include: { tasks: true } }>,
+      'tasks'
+    >
+  ): Member {
+    const raw = nullsToUndefined(raw_)
+
+    return Member.create({ name: raw.name, email: raw.email, password: raw.password }, raw.id)
+  }
+
+  static toPersistence(member: Member): Prisma.MemberCreateInput {
+    return {
+      name: member.name,
+      email: member.email,
+      password: member.password
+    }
+  }
+}
