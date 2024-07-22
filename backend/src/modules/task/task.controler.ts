3568328a22051253c3d08f1f7a13e1b14b20c9f1
Felipo <gabriel.felipo2010@gmail.com>
2024-07-22 15:29:26 -0300

    feat(update-task): add controler

--- backend/src/modules/task/task.controler.ts
+++ backend/src/modules/task/task.controler.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Patch, Post } from '@nestjs/common'
 
 import { createTaskSchema } from './dtos/create-task'
 import { ZodValidationPipe } from 'src/utils/validation-pipe'
 import { CreateTaskDto } from './dtos/create-task'
 import { RegisterTaskUseCase } from './use-cases/register-task'
+import { UpdateTaskDto, updateTaskSchema } from './dtos/update-task'
+import { UpdateTaskUseCase } from './use-cases/update-task'
 
 @Controller('tasks')
 export class TaskController {
-  constructor(private registerTaskUseCase: RegisterTaskUseCase) {}
+  constructor(
+    private registerTaskUseCase: RegisterTaskUseCase, 
+    private updateTaskUseCase: UpdateTaskUseCase
+  ) {}
 
   @Post()
   async createTask(
@@ -15,5 +20,13 @@ export class TaskController {
     createTaskDto: CreateTaskDto
   ) {
     return await this.registerTaskUseCase.execute(createTaskDto, '5728be95-81cb-47cc-b829-e9d77a833ebd')
+  }
+
+  @Patch()
+  async updateTask(
+    @Body(new ZodValidationPipe(updateTaskSchema))
+    updateTaskDto: UpdateTaskDto
+  ) {
+    return await this.updateTaskUseCase.execute(updateTaskDto)
   }
 }
--- backend/src/modules/task/task.module.ts
+++ backend/src/modules/task/task.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common'
 import { PrismaModule } from 'src/common/prisma/prisma.module';
 import { TaskController } from './task.controler';
 import { RegisterTaskUseCase } from './use-cases/register-task';
+import { UpdateTaskUseCase } from './use-cases/update-task';
 
 
 @Module({
   imports: [PrismaModule],
   controllers: [TaskController],
-  providers: [RegisterTaskUseCase], 
+  providers: [RegisterTaskUseCase, UpdateTaskUseCase], 
 })
 export class TaskModule {}
