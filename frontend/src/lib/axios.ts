f2aef0b4a46154cf7c2546bb49a2831fe20e621f
Felipo <gabriel.felipo2010@gmail.com>
2024-07-22 19:34:55 -0300

    feat(initial-config-frontend): add axios and react-query

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,54 @@
+import axios, { AxiosError } from 'axios'
+import { config } from '~/config'
+
+export const axiosInstance = axios.create({
+  baseURL: config.apiUrl,
+})
+
+const getAuthHeader = () => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    return `Bearer ${token}`
+  }
+}
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = getAuthHeader()
+    return config
+  },
+  (error) => Promise.reject(error),
+)
+
+export function setAuthorizationToken(token: string, refreshToken: string) {
+  localStorage.setItem('token', token)
+}
+
+export type IApiResponse<T> = {
+  message: string
+  response: T
+}
+
+export type RawApiError = AxiosError<{
+  code: number
+  message: string
+  response: Record<string, unknown>
+}>
+
+export class ApiError extends Error {
+  httpStatus?: number
+  code: number
+  errorType: string
+  response: {
+    [key: string]: unknown
+  }
+
+  constructor(error: RawApiError) {
+    super()
+    this.name = 'ApiError'
+    this.httpStatus = error.response?.status
+    this.code = error.response?.data?.code!
+    this.errorType = error.response?.data?.message!
+    this.response = error.response?.data?.response!
+  }
+}
