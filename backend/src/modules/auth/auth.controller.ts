13e0e7c9229e771c498d198055a0034dd0d65d2f
Felipo <gabriel.felipo2010@gmail.com>
2024-07-23 22:39:09 -0300

    feat(home): make controller

---
+++ backend/src/modules/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+import { IsPublic } from './decorators/is-public.decorator';
+import { AuthRequest } from './models/auth-request';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @IsPublic()
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Request() req: AuthRequest) {
+    return this.authService.login(req.user);
+  }
+}
