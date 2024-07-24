eb1ba45268e1cb6cf0b144f786c1144c88f27669
Felipo <gabriel.felipo2010@gmail.com>
2024-07-23 22:48:56 -0300

    feat(tasks): format project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
+import tspaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tspaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
