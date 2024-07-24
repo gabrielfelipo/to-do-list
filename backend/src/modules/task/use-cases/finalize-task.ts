05f1a9074275297dcb64cef22f58af4726caa618
Felipo <gabriel.felipo2010@gmail.com>
2024-07-23 22:47:16 -0300

    feat(tasks): change finalized type on finalizeTask usecase

--- backend/src/modules/task/use-cases/finalize-task.ts
+++ backend/src/modules/task/use-cases/finalize-task.ts
@@ -8,6 +8,7 @@ import { NoneElementError } from 'src/common/errors/none-element-error'
 import { GetTaskDto } from '../dtos/get-task'
 import { FinalizeTaskDto } from '../dtos/finalize-task'
 import dayjs from 'dayjs'
+import { Task } from '../entities/Task'
 
 type FinalizeTaskResponse = AsyncEither<DomainError, IUseCaseResponse>
 
@@ -16,16 +17,27 @@ export class FinalizeTaskUseCase implements IUseCase {
   constructor(private readonly taskRepository: TaskRepository) {}
 
   async execute(payload: FinalizeTaskDto): FinalizeTaskResponse {
-    const { id: taskId } = payload
+    const { id: taskId , finalized: isFinalized} = payload
 
     const task = await this.taskRepository.findById(taskId)
     if (!task) return left(new NoneElementError('Unfounded task'))
+    
+    let finalizedTask: Task
 
-    const finalizedTask = await this.taskRepository.update({
+    if (isFinalized) {
+      finalizedTask = await this.taskRepository.update({
         id: task.id,
         finalized: true,
         endDate: dayjs().toDate()
-    })
+      })
+    }
+    else {
+      finalizedTask = await this.taskRepository.update({
+        id: task.id,
+        finalized: false,
+        endDate: undefined
+      })
+    }
 
     return right({
       message: 'Task finalized successfuly',
